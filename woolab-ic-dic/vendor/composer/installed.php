384b8d59a59f1a6eb13f0e9058c3189df032fb8c
Sean Murphy <sean@iamseanmurphy.com>
2026-03-19 13:43:21 +0000

    1.10.4

--- woolab-ic-dic/vendor/composer/installed.php
+++ woolab-ic-dic/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'vyskocilova/kybernaut-ic-dic',
-        'pretty_version' => '1.10.3',
-        'version' => '1.10.3.0',
-        'reference' => '21a4a93f846172931af8b55e7ff78c637609dced',
+        'pretty_version' => '1.10.4',
+        'version' => '1.10.4.0',
+        'reference' => 'ea936798f95e829f7bb5ea835a7206b973a15e78',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'vyskocilova/kybernaut-ic-dic' => array(
-            'pretty_version' => '1.10.3',
-            'version' => '1.10.3.0',
-            'reference' => '21a4a93f846172931af8b55e7ff78c637609dced',
+            'pretty_version' => '1.10.4',
+            'version' => '1.10.4.0',
+            'reference' => 'ea936798f95e829f7bb5ea835a7206b973a15e78',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
